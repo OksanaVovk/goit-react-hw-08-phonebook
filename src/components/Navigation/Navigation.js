be263c93ebdc52b873d068da59f81bc3541d2ee8
OksanaVovk <oksvovk85@gmail.com>
2022-09-30 13:33:59 +0300

    Update Navigation.js

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -8,7 +8,7 @@ const Navigation = () => {
   const isLoggedIn = useSelector(authSelector.getIsLoggedIn);
   return (
     <Box as="nav" display="flex" flexDirection="row" gridGap="20px">
-      <LinkNav to="/goit-react-hw-08-phonebook">Home</LinkNav>
+      <LinkNav to="/">Home</LinkNav>
       {isLoggedIn && <LinkNav to="/contacts">Contacts</LinkNav>}
     </Box>
   );
